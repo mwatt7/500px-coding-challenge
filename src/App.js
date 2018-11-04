22d5a888be21b5e8ae6052673a4301ea4dff79f5
mwatt7 <mwatt@outlook.com>
2018-11-04 08:20:23 -0500

    Implement GET request to /photos action of 500px API
    - Most basic implementation of 500px API
    - Returns most popular photos, maps image_url to a state array called 'photos'
    - Prints list of urls to the browser without any style or formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const api_key_500px = process.env.REACT_APP_500PX_API_KEY
+const api_root_query = "https://api.500px.com/v1/photos"
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -8,9 +11,26 @@ class App extends Component {
       photos: [],
     }
   }
-  render() {
-    return (<div></div>);
+
+  build_query(){
+    let query = api_root_query + "?";
+    query += "consumer_key=" + api_key_500px;
+    query += "&feature=popular";
+
+    return query;
+
   }
+
+  componentDidMount() {
+    fetch( this.build_query() )
+      .then(response => response.json())
+      .then(data => this.setState({ photos: data.photos.map( p => p.image_url) }))
+  }
+
+  render() {
+    return (<div>{this.state.photos}</div>);
+  }
+
 }
 
 export default App;
